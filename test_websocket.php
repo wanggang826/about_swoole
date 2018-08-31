88063f384bf61701f53bf22654d6d20efd4b65f4
wgg <wgg@carkusoft.com>
2018-08-31 18:32:49 +0800

    fix bug index css

--- test_websocket.php
+++ test_websocket.php
@@ -9,7 +9,7 @@ if(!$user){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
-    <title>webSocket</title>
+    <title><?php echo $user;?></title>
 </head>
 <body>
 <div style="text-align:center">
