dd4c6f8997e105d49a4a9ad4f7b8948e41aca990
wgg <wgg@carkusoft.com>
2018-09-01 15:56:02 +0800

    chat html

---
+++ chat.php
@@ -0,0 +1,158 @@
+<?php
+$user = $_POST['name'];
+if(!$user){
+    header('Location:http://sw.wanggangg.top/');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>chat</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+    <link rel="stylesheet" href="css/chat.css" type="text/css">
+    <script type="text/javascript" src="js/chat.js"></script>
+</head>
+<body>
+<div class="all">
+    <div class="chat_index">
+        <!--banner-->
+        <div class="chat_banner">
+
+        </div>
+
+        <div class="chat_body">
+            <!--在线列表-->
+            <div class="chat_online">
+                <!--搜索-->
+                <div class="search_online">
+                    <form>
+                        <input type="text" placeholder="搜索联系人">
+                    </form>
+                </div>
+                <div class="online_friend">
+                    <ul>
+                        <li>
+                            <div class="a_friend">
+                                <div class="head_portrait">
+                                    <div class="head_text">
+                                        黄
+                                    </div>
+                                </div>
+                                <div class="friend">
+
+                                    <div class="name">天狼星</div>
+                                    <div class="this_time">4-12-15:10</div>
+
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="a_friend">
+                                <div class="head_portrait">
+                                    <div class="head_text">
+                                        黄
+                                    </div>
+                                </div>
+                                <div class="friend">
+
+                                    <div class="name">天狼星</div>
+                                    <div class="this_time">4-12-15:10</div>
+
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="a_friend">
+                                <div class="head_portrait">
+                                    <div class="head_text">
+                                        黄
+                                    </div>
+                                </div>
+                                <div class="friend">
+
+                                    <div class="name">天狼星</div>
+                                    <div class="this_time">4-12-15:10</div>
+
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="a_friend">
+                                <div class="head_portrait">
+                                    <div class="head_text">
+                                        黄
+                                    </div>
+                                </div>
+                                <div class="friend">
+
+                                    <div class="name">天狼星</div>
+                                    <div class="this_time">4-12-15:10</div>
+
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+            <!--聊天界面-->
+            <div class="chat_main">
+                <div class="chat_div" id="div">
+<!--                    <ul id="chat_ul" class="chat_content">-->
+<!---->
+<!--                    </ul>-->
+
+                </div>
+
+                <div class="send_message">
+                    <form>
+                        <input type="text" placeholder="请输入消息" id="send_txt">
+                        <input type="button" value="发送" id="send_btn" onclick="sendMassage('all')">
+                    </form>
+                </div>
+            </div>
+            <!--名片-->
+            <div class="chat_namecard">
+
+            </div>
+        </div>
+
+    </div>
+</div>
+</body>
+<script src="js/jquery-1.8.2.min.js"></script>
+<script>
+    var wsServer = 'ws://47.95.236.88:9999';//这里的IP应该更改
+    //    var wsServer = 'ws://47.95.236.88:9988';//这里的IP应该更改
+    //    var wsServer = 'ws://10.10.10.11:9988';//这里的IP应该更改
+    var websocket = new WebSocket(wsServer);
+    websocket.onopen = function (evt) {
+        console.log("Connected to WebSocket server.");
+        websocket.send('{"user":"<?php echo $user;?>" ,"type":"1"}')
+    };
+
+    websocket.onclose = function (evt) {
+        console.log("Disconnected");
+    };
+
+    websocket.onmessage = function (evt) {
+        $('#div').append(evt.data+"<br>");
+        $('#div').scrollTop($('#div')[0].scrollHeight);
+        // document.getElementById('div').style.background = evt.data;
+        console.log('Retrieved data from server: ' + evt.data);
+    };
+
+    websocket.onerror = function (evt, e) {
+        console.log('Error occured: ' + evt.data);
+    };
+    function sendMassage(to_user){
+        var massage=document.getElementById('send_txt').value;
+        var msg = '{"type":"2","msg":"'+massage+'","from_user":"<?php echo $user;?>","to_user":"'+to_user+'"}';
+        websocket.send(msg);
+        $('#send_txt').val('');
+    }
+
+</script>
+
+</html>
--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 <div style="background: #60fff4;text-align: center;height: 450px;">
-<form action="test_websocket.php" method="post" style="display: inline-block;overflow:auto;margin-top: 180px;">
+<form action="chat.php" method="post" style="display: inline-block;overflow:auto;margin-top: 180px;">
 输入用户名<input type="text" name="name" value="" style="height: 25px">
         <input type="submit" value="提交" style="height: 30px">
 </div>
